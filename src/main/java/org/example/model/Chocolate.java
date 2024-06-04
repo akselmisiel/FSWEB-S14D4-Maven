9dcb6afcc90d042a290bf80f7088aa1e242283fb
Aksel <akselmisiel@gmail.com>
2024-06-04 21:28:31 +0300

    Done

---
+++ src/main/java/org/example/model/Chocolate.java
@@ -0,0 +1,22 @@
+package org.example.model;
+
+public class Chocolate extends ProductForSale{
+     private String chocolateType;
+
+    public Chocolate(String type, double price, String description) {
+        super(type, price, description);
+    }
+
+    public Chocolate(String type, double price, String description, String chocolateType) {
+        super(type, price, description);
+        this.chocolateType = chocolateType;
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("Type: " + getType());
+        System.out.println("Price: " + getPrice());
+        System.out.println("Description: " + getDescription());
+        System.out.println("Chocolate Type: " + chocolateType);
+    }
+}
--- src/main/java/org/example/model/Store.java
+++ src/main/java/org/example/model/Store.java
@@ -2,11 +2,17 @@ package org.example.model;
 
 public class Store {
     public static void main(String[] args) {
-
-        //listProducts();
+        ProductForSale[] productList = {
+                new Coke("Coke", 1.5, "Coca Cola", true),
+                new Bread("Bread", 2.5, "Whole Wheat", 0.5),
+                new Chocolate("Chocolate", 3.5, "Dark Chocolate", "Dark")
+        };
+        listProducts(productList);
     }
 
     public static void listProducts(ProductForSale[] products) {
-
+        for (ProductForSale product : products) {
+            product.showDetails();
+        }
     }
 }
